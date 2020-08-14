d68616e7813efdda9786c87a4892a2e4454cd3f6
Pulkit Bhardwaj <pulkitbhardwaj1994@gmail.com>
2020-08-15 05:06:12 +0530

    Updates

---
+++ precache-manifest.1c6c127901955abe289b23876f3d1d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5107da84cd4b19631821284367846a47",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "d3553c66040b443c4206",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "d3553c66040b443c4206",
+    "url": "/covid-tracker/static/js/2.8e425a88.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.8e425a88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f3f18c375b60897ab15",
+    "url": "/covid-tracker/static/js/main.bcba315f.chunk.js"
+  },
+  {
+    "revision": "d18efbfbd5f9bcddd6ff",
+    "url": "/covid-tracker/static/js/runtime-main.fe48da39.js"
+  }
+]);
